1d85367904a61fee0dedeb8585f46497e8a521a0
root <root@DESKTOP-0T01DAB>
2023-06-11 22:05:40 +0800

    add neoscroll plugin

--- configs/ui.lua
+++ configs/ui.lua
@@ -188,4 +188,13 @@ return {
     "p00f/nvim-ts-rainbow",
     lazy = false,
   },
+
+  -- scroll
+  {
+    "karb94/neoscroll.nvim",
+    keys = { "<C-d>", "<C-u>" },
+    config = function()
+      require("neoscroll").setup()
+    end,
+  },
 }
