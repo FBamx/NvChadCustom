61463597e93512708532b2ad87f9a556ab4ceea4
root <root@DESKTOP-0T01DAB>
2023-06-12 22:00:30 +0800

    add crate.nvim

--- plugins.lua
+++ plugins.lua
@@ -333,26 +333,32 @@ local plugins = {
     end,
   },
 
-  -- rust toos
+  -- rust tools
   {
     "simrat39/rust-tools.nvim",
-    lazy = false,
-    opts = {
-      -- automatically call RustReloadWorkspace when writing to a Cargo.toml file.
-      reload_workspace_from_cargo_toml = true,
-      inlay_hints = {
-        auto = true,
-        only_current_line = false,
-        show_parameter_hints = true,
-        parameter_hints_prefix = "<- ",
-        other_hints_prefix = "=> ",
-        max_len_align = false,
-        max_len_align_padding = 1,
-        right_align = false,
-        right_align_padding = 7,
-        highlight = "Comment",
-      },
-    },
+    ft = "rust",
+    dependencies = "neovi/nvim-lspconfig",
+    config = function()
+      local on_attach = require("plugins.configs.lspconfig").on_attach
+      local capabilities = require("plugins.configs.lspconfig").capabilities
+      require("rust-tools").setup {
+        server = {
+          on_attach = on_attach,
+          capabilities = capabilities,
+        },
+      }
+    end,
+  },
+
+  -- crates
+  {
+    "saecki/crates.nvim",
+    ft = { "rust", "toml" },
+    config = function(_, opts)
+      local crate = require "crates"
+      crate.setup(opts)
+      crate.show()
+    end,
   },
 
   { import = "custom.configs.telescope" },
