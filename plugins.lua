a9e4bc39ec1606a61e467dc7e7f904b349028670
root <root@DESKTOP-0T01DAB>
2023-06-12 10:41:42 +0800

    add rust tools

--- plugins.lua
+++ plugins.lua
@@ -333,6 +333,28 @@ local plugins = {
     end,
   },
 
+  -- rust toos
+  {
+    "simrat39/rust-tools.nvim",
+    lazy = false,
+    opts = {
+      -- automatically call RustReloadWorkspace when writing to a Cargo.toml file.
+      reload_workspace_from_cargo_toml = true,
+      inlay_hints = {
+        auto = true,
+        only_current_line = false,
+        show_parameter_hints = true,
+        parameter_hints_prefix = "<- ",
+        other_hints_prefix = "=> ",
+        max_len_align = false,
+        max_len_align_padding = 1,
+        right_align = false,
+        right_align_padding = 7,
+        highlight = "Comment",
+      },
+    },
+  },
+
   { import = "custom.configs.telescope" },
   { import = "custom.configs.ui" },
   { import = "custom.configs.dap" },
